554cca233ce5b2e5b28f412385e388618fc0685f
Bünyamin Epatay <epatayb@gmail.com>
2025-03-13 17:01:04 +0300

    programın algoritması tamamlandı.

---
+++ roman_rakam.py
@@ -0,0 +1,26 @@
+def to_number(roman):
+    numbers = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    total = 0
+    length = len(roman)
+
+    for i in range(length-1):
+        if (numbers[roman[i]] < numbers[roman[i+1]]):
+            total -= numbers[roman[i]]
+        else:
+            total += numbers[roman[i]]
+
+    total += numbers[roman[length-1]]
+    return total
+
+
+print(to_number("MCMVII"))
+
